ac6db3a3b045e59a1eb0b9283252d498afb7b88f
NyiNyiNayNaing <C5224@oic.jp>
2026-02-01 15:29:50 +0900

    Add project files.

---
+++ LeaveMangementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveMangementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveMangementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult TestPage()
+        {
+            var data = new TestViewModel()
+            {
+                Name = "Test",
+                Age = 1,
+            };
+
+            return View(data);
+        }
+    }
+}
---
+++ LeaveMangementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveMangementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
